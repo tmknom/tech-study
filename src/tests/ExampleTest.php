301e8cb7de3fd8db3eabc1654cee7e8dd171782d
tmknom <tmk.nom@gmail.com>
2015-02-08 01:06:52 +0900

    DBのテストのサンプルを追加

--- src/tests/ExampleTest.php
+++ src/tests/ExampleTest.php
@@ -14,4 +14,20 @@ class ExampleTest extends TestCase {
 		$this->assertEquals(200, $response->getStatusCode());
 	}
 
+    public function testDbExample()
+    {
+
+        // 実行
+        $insertArray = array(
+            'name' => '森鴎外',
+            'email' => 'hoge@example.com',
+            'password' => 'new_password',
+        );
+        $id = DB::table('users')->insertGetId($insertArray);
+
+        // 確認
+        $result = DB::table('users')->where('id', '=', $id)->first();
+        $this->assertEquals('森鴎外', $result->name);
+    }
+
 }
