9b8fe3969d03cb69e4db5c298ad61b0db0f96c56
tmknom <tmk.nom@gmail.com>
2015-02-08 16:28:20 +0900

    TestCaseにDBのマイグレーション処理を追加

--- src/tests/TestCase.php
+++ src/tests/TestCase.php
@@ -2,6 +2,39 @@
 
 class TestCase extends Illuminate\Foundation\Testing\TestCase {
 
+    /** @before */
+    public function setUp()
+    {
+        parent::setUp();
+
+        $this->setUpDb();
+    }
+
+    /** @after */
+    public function tearDown()
+    {
+        Mockery::close();
+        $this->tearDownDb();
+
+        parent::tearDown();
+    }
+
+    /** DBの準備 */
+    private function setUpDb()
+    {
+        // テーブル作成
+        Artisan::call('migrate');
+    }
+
+    /** DBの後始末 */
+    private function tearDownDb()
+    {
+        // テーブル削除
+        Artisan::call('migrate:reset');
+        // DB切断
+        DB::disconnect();
+    }
+
 	/**
 	 * Creates the application.
 	 *
