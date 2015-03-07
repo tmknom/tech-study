b829b1d5cc65c10046e4b13b373bf9c708155e34
tmknom <tmk.nom@gmail.com>
2015-03-07 17:18:17 +0900

    ExampleTestのコメントアウトしていたテスト復活

--- src/tests/ExampleTest.php
+++ src/tests/ExampleTest.php
@@ -2,14 +2,14 @@
 
 class ExampleTest extends TestCase {
 
-//	/** @test */
-//	public function testBasicExample()
-//	{
-//		$response = $this->call('GET', '/');
-//
-//		$this->assertEquals(200, $response->getStatusCode());
-//        //$this->assertEquals(true, false);
-//	}
+	/** @test */
+	public function testBasicExample()
+	{
+		$response = $this->call('GET', '/');
+
+		$this->assertEquals(200, $response->getStatusCode());
+        //$this->assertEquals(true, false);
+	}
 
     /** @test */
     public function dbExample()
