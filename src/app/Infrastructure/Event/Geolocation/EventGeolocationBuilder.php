352ac13b894f7c10b7beace104c04807b0134b98
tmknom <tmk.nom@gmail.com>
2015-05-06 12:46:28 +0900

    EventGeolocationBuilder作成

---
+++ src/app/Infrastructure/Event/Geolocation/EventGeolocationBuilder.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Infrastructure\Event\Geolocation;
+
+use App\Domain\Event\Geolocation\Address;
+use App\Domain\Event\Geolocation\EventGeolocation;
+use App\Domain\Event\Geolocation\Latitude;
+use App\Domain\Event\Geolocation\Longitude;
+use App\Domain\Event\Geolocation\Place;
+
+class EventGeolocationBuilder
+{
+
+    /** @var Address */
+    private $address;
+
+    /** @var Place */
+    private $place;
+
+    /** @var Latitude */
+    private $latitude;
+
+    /** @var Longitude */
+    private $longitude;
+
+    public function setAddress($value)
+    {
+        $this->address = new Address($this->getValueOrEmpty($value));
+        return $this;
+    }
+
+    public function setPlace($value)
+    {
+        $this->place = new Place($this->getValueOrEmpty($value));
+        return $this;
+    }
+
+    public function setLatitude($value)
+    {
+        $this->latitude = new Latitude($this->getValueOrNull($value));
+        return $this;
+    }
+
+    public function setLongitude($value)
+    {
+        $this->longitude = new Longitude($this->getValueOrNull($value));
+        return $this;
+    }
+
+    /** @return EventGeolocation */
+    public function build()
+    {
+        return new EventGeolocation(
+                $this->address, $this->place, $this->latitude, $this->longitude
+        );
+    }
+
+    private function getValueOrEmpty($value)
+    {
+        return empty($value) ? '' : $value;
+    }
+
+    private function getValueOrNull($value)
+    {
+        return empty($value) ? null : $value;
+    }
+
+    /** @return EventGeolocationBuilder */
+    public static function builder()
+    {
+        return new EventGeolocationBuilder();
+    }
+
+    /** コンストラクタ */
+    private function __construct()
+    {
+        
+    }
+
+}
