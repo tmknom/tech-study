0097962112bbd2c398c34bdf00d68f04241147bd
tmknom <tmk.nom@gmail.com>
2015-05-06 09:04:27 +0900

    Entity識別子用のtrait作成

---
+++ src/app/Library/Domain/Identifier.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Library\Domain;
+
+trait Identifier {
+
+    use \App\Library\Domain\ValueObject;
+
+    public static function createUndefinedInstance(){
+        $class = static::class;
+        return new $class(self::getUndefined());
+    }
+
+    private static function getUndefined(){
+        return 'undefined';
+    }
+
+}
