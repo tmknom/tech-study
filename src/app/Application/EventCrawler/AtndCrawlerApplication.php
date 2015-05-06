3f1141a6e7aef2035b935f320930b95225f0c443
tmknom <tmk.nom@gmail.com>
2015-05-06 21:40:47 +0900

    TransactionCrawlerApplication作成

--- src/app/Application/EventCrawler/AtndCrawlerApplication.php
+++ src/app/Application/EventCrawler/AtndCrawlerApplication.php
@@ -2,47 +2,18 @@
 
 namespace App\Application\EventCrawler;
 
-use App\Domain\Event\EventList;
 use App\Domain\Event\EventRepository;
 use App\Domain\EventCrawler\AtndCrawler;
-use DB;
 
 class AtndCrawlerApplication
 {
 
-    /** @var AtndCrawler */
-    private $atndCrawler;
-
-    /**  @var EventRepository */
-    private $eventRepository;
-
-    /**  @var EventList */
-    private $savedEventList;
+    use TransactionCrawlerApplication;
 
     /** コンストラクタ */
-    public function __construct(AtndCrawler $atndCrawler, EventRepository $eventRepository)
+    public function __construct(AtndCrawler $crawler, EventRepository $eventRepository)
     {
-        $this->atndCrawler = $atndCrawler;
-        $this->eventRepository = $eventRepository;
-        $this->savedEventList = null;
-    }
-
-    /**
-     * @return EventList
-     */
-    public function crawl()
-    {
-        DB::transaction(function () {
-            //APIのリクエスト
-            $eventList = $this->atndCrawler->crawl();
-
-            // MySQLへの登録
-            $this->savedEventList = $this->eventRepository->saveAll($eventList);
-
-            // Elasticsearchへの登録
-        });
-
-        return $this->savedEventList;
+        $this->constructByCaller($crawler, $eventRepository);
     }
 
 }
---
+++ src/app/Application/EventCrawler/TransactionCrawlerApplication.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Application\EventCrawler;
+
+use App\Domain\Event\EventList;
+use App\Domain\Event\EventRepository;
+use App\Domain\EventCrawler\EventCrawler;
+use DB;
+
+trait TransactionCrawlerApplication
+{
+
+    /**  @var EventCrawler */
+    private $crawler;
+
+    /**  @var EventRepository */
+    private $eventRepository;
+
+    /**  @var EventList */
+    private $savedEventList;
+
+    /** 呼び出し元クラスから呼び出すコンストラクタ */
+    private function constructByCaller(EventCrawler $crawler, EventRepository $eventRepository)
+    {
+        $this->crawler = $crawler;
+        $this->eventRepository = $eventRepository;
+        $this->savedEventList = null;
+    }
+
+    /**
+     * @return EventList
+     */
+    public function crawl()
+    {
+        DB::transaction(function () {
+            // JSON APIのリクエスト
+            $eventList = $this->crawler->crawl();
+
+            // MySQLへの登録
+            $this->savedEventList = $this->eventRepository->saveAll($eventList);
+
+            // Elasticsearchへの登録
+        });
+
+        return $this->savedEventList;
+    }
+
+}
