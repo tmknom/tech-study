2c36bf9b00eee613793513c34ceba91ccebbc601
tmknom <tmk.nom@gmail.com>
2015-05-06 12:31:34 +0900

    EventCapacityBuilder作成

---
+++ src/app/Infrastructure/Event/Capacity/EventCapacityBuilder.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Infrastructure\Event\Capacity;
+
+use App\Domain\Event\Capacity\Accepted;
+use App\Domain\Event\Capacity\CapacityLimit;
+use App\Domain\Event\Capacity\EventCapacity;
+use App\Domain\Event\Capacity\Waiting;
+
+class EventCapacityBuilder
+{
+
+    /** @var CapacityLimit */
+    private $capacityLimit;
+
+    /** @var Accepted */
+    private $accepted;
+
+    /** @var Waiting */
+    private $waiting;
+
+    public function setCapacityLimit($value)
+    {
+        $this->capacityLimit = new CapacityLimit($this->getValueOrZero($value));
+        return $this;
+    }
+
+    public function setAccepted($value)
+    {
+        $this->accepted = new Accepted($this->getValueOrZero($value));
+        return $this;
+    }
+
+    public function setWaiting($value)
+    {
+        $this->waiting = new Waiting($this->getValueOrZero($value));
+        return $this;
+    }
+
+    /** @return EventCapacity */
+    public function build()
+    {
+        return new EventCapacity(
+                $this->capacityLimit, $this->accepted, $this->waiting
+        );
+    }
+
+    private function getValueOrZero($value)
+    {
+        return empty($value) ? 0 : $value;
+    }
+
+    /** @return EventCapacityBuilder */
+    public static function builder()
+    {
+        return new EventCapacityBuilder();
+    }
+
+    /** コンストラクタ */
+    private function __construct()
+    {
+        
+    }
+
+}
