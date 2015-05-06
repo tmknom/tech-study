c02799e3c655e01d79dfb54a7ce3a76ee5cb54e6
tmknom <tmk.nom@gmail.com>
2015-05-06 12:45:28 +0900

    EventGeolocation作成

---
+++ src/app/Domain/Event/Geolocation/EventGeolocation.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Domain\Event\Geolocation;
+
+use App\Library\Domain\Entity;
+
+class EventGeolocation
+{
+
+    use Entity;
+
+    /** @var Address */
+    private $address;
+
+    /** @var Place */
+    private $place;
+
+    /** @var Latitude */
+    private $latitude;
+
+    /** @var Longitude */
+    private $longitude;
+
+    public function __construct(Address $address, Place $place, Latitude $latitude,
+                                Longitude $longitude)
+    {
+
+        $this->completeConstruct(func_get_args());
+    }
+
+    /**
+     * @return Address
+     */
+    public function getAddress()
+    {
+        return $this->address;
+    }
+
+    /**
+     * @return Latitude
+     */
+    public function getLatitude()
+    {
+        return $this->latitude;
+    }
+
+    /**
+     * @return Longitude
+     */
+    public function getLongitude()
+    {
+        return $this->longitude;
+    }
+
+    /**
+     * @return Place
+     */
+    public function getPlace()
+    {
+        return $this->place;
+    }
+
+}
