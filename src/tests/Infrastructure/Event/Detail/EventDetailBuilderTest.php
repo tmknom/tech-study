adb6ebd6e675235303db754137ca4a7ba5799d73
tmknom <tmk.nom@gmail.com>
2015-05-06 14:07:22 +0900

    EventDetailBuilderTest作成

---
+++ src/tests/Infrastructure/Event/Detail/EventDetailBuilderTest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Tests\Infrastructure\Event\Capacity;
+
+use App\Domain\Event\Detail\CatchCopy;
+use App\Domain\Event\Detail\EventDescription;
+use App\Domain\Event\Detail\EventDetail;
+use App\Domain\Event\Detail\OwnerId;
+use App\Domain\Event\Detail\SourceEventId;
+use App\Infrastructure\Event\Detail\EventDetailBuilder;
+use PHPUnit_Framework_TestCase;
+
+class EventDetailBuilderTest extends PHPUnit_Framework_TestCase
+{
+
+    /** @test */
+    public function build_正常系()
+    {
+        // 実行
+        $actual = EventDetailBuilder::builder()
+                ->setCatchCopy('キャッチコピー1')
+                ->setEventDescription('概要1')
+                ->setOwnerId('owner1')
+                ->setSourceEventId('source_id1')
+                ->build();
+
+        // 確認
+        $expected = new EventDetail(
+                new SourceEventId('source_id1'), 
+                new EventDescription('概要1'),
+                new CatchCopy('キャッチコピー1'),
+                new OwnerId('owner1')
+        );
+        $this->assertEquals($expected, $actual);
+    }
+
+}
