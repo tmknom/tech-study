2c85b2fbf6bd29651e7052c1992aa9202b14c8bb
tmknom <tmk.nom@gmail.com>
2015-05-06 13:14:56 +0900

    EventBuilder作成

---
+++ src/app/Infrastructure/Event/EventBuilder.php
@@ -0,0 +1,107 @@
+<?php
+
+namespace App\Infrastructure\Event;
+
+use App\Domain\Event\Area\EventArea;
+use App\Domain\Event\Capacity\EventCapacity;
+use App\Domain\Event\Core\EventCore;
+use App\Domain\Event\Detail\EventDetail;
+use App\Domain\Event\Event;
+use App\Domain\Event\EventId;
+use App\Domain\Event\Geolocation\EventGeolocation;
+use App\Domain\Event\Rating\EventRating;
+
+class EventBuilder
+{
+
+    /** @var EventId */
+    private $eventId;
+
+    /** @var EventCore */
+    private $eventCore;
+
+    /** @var EventDetail */
+    private $eventDetail;
+
+    /** @var EventCapacity */
+    private $eventCapacity;
+
+    /** @var EventRating */
+    private $eventRating;
+
+    /** @var EventGeolocation */
+    private $eventGeolocation;
+
+    /** @var EventArea */
+    private $eventArea;
+
+    public function setUndefinedEventId()
+    {
+        $this->eventId = EventId::createUndefinedInstance();
+        return $this;
+    }
+
+    public function setEventId($value)
+    {
+        $this->eventId = new EventId($value);
+        return $this;
+    }
+
+    public function setEventCore(EventCore $value)
+    {
+        $this->eventCore = $value;
+        return $this;
+    }
+
+    public function setEventDetail(EventDetail $value)
+    {
+        $this->eventDetail = $value;
+        return $this;
+    }
+
+    public function setEventCapacity(EventCapacity $value)
+    {
+        $this->eventCapacity = $value;
+        return $this;
+    }
+
+    public function setEventRating(EventRating $value)
+    {
+        $this->eventRating = $value;
+        return $this;
+    }
+
+    public function setEventGeolocation(EventGeolocation $value)
+    {
+        $this->eventGeolocation = $value;
+        return $this;
+    }
+
+    public function setEventArea(EventArea $eventArea)
+    {
+        $this->eventArea = $eventArea;
+        return $this;
+    }
+
+    /** @return Event */
+    public function build()
+    {
+        return new Event(
+                $this->eventId, $this->eventCore, $this->eventDetail, $this->eventCapacity,
+                $this->eventRating, $this->eventGeolocation, $this->eventArea
+        );
+    }
+
+    /** @return EventBuilder */
+    public static function builder()
+    {
+        return new EventBuilder();
+    }
+
+    /** コンストラクタ */
+    private function __construct()
+    {
+        
+    }
+
+}
