eaf2efba3d1407b41a49ad419f13d9c935bd58ea
tmknom <tmk.nom@gmail.com>
2015-05-06 16:18:01 +0900

    AtndMapper作成

--- src/app/Infrastructure/EventCrawler/RestAtndCrawler.php
+++ src/app/Infrastructure/EventCrawler/RestAtndCrawler.php
@@ -3,28 +3,32 @@
 namespace App\Infrastructure\EventCrawler;
 
 use App\Domain\EventCrawler\AtndCrawler;
+use App\Infrastructure\EventCrawler\Mapper\AtndMapper;
 use App\Library\Http\JsonHttpClient;
 
 class RestAtndCrawler implements AtndCrawler
 {
 
+    /** @var string APIのURL */
+    const URL = 'http://api.atnd.org/events/?format=json&count=30';
+
     /** @var JsonHttpClient */
     private $jsonHttpClient;
 
-    /** @var string APIのURL */
-    const URL = 'http://api.atnd.org/events/?format=json&count=30';
+    /** @var AtndMapper */
+    private $atndMapper;
 
     /** コンストラクタ */
     public function __construct(JsonHttpClient $jsonHttpClient)
     {
         $this->jsonHttpClient = $jsonHttpClient;
+        $this->atndMapper = new AtndMapper();
     }
 
     public function crawl()
     {
         $json = $this->jsonHttpClient->request(self::URL);
-        return $json;
-//        return $this->atndJsonMapper->createEventList($this->requestApi());
+        return $this->atndMapper->createEventList($json);
     }
 
 }
--- src/tests/Infrastructure/EventCrawler/RestAtndCrawlerTest.php
+++ src/tests/Infrastructure/EventCrawler/RestAtndCrawlerTest.php
@@ -2,9 +2,12 @@
 
 namespace Tests\Infrastructure\EventCrawler;
 
-use App\Infrastructure\EventCrawler\RestSampleCrawler;
+use App\Domain\Event\Core\SourceType;
+use App\Domain\Event\EventList;
 use App\Infrastructure\EventCrawler\RestAtndCrawler;
+use App\Infrastructure\EventCrawler\RestSampleCrawler;
 use PHPUnit_Framework_TestCase;
+use Symfony\Component\EventDispatcher\Event;
 use Tests\Infrastructure\EventCrawler\Stub\AtndHttpClient;
 
 class RestAtndCrawlerTest extends PHPUnit_Framework_TestCase
@@ -26,8 +29,37 @@ class RestAtndCrawlerTest extends PHPUnit_Framework_TestCase
         $actual = $this->sut->crawl();
 
         // 確認
-        $this->assertCount(3, $actual['events']);
-        $this->assertEquals('サンプルイベント1', $actual['events'][0]['event']['title']);
+        // 確認：全件チェック
+        $this->assertTrue($actual instanceof EventList);
+        $this->assertCount(2, $actual->getValue());
+
+        // 確認：１件チェック
+        /** @var Event $event */
+        $event = $actual->getValue()[0];
+        $this->assertEquals("undefined", $event->getEventId());
+
+        $eventCore = $event->getEventCore();
+        $this->assertEquals("サンプルイベント1", $eventCore->getEventTitle());
+        $this->assertEquals("http://atnd.org/events/17662", $eventCore->getEventUrl());
+        $this->assertEquals(SourceType::ATND, $eventCore->getSourceType());
+        $this->assertEquals("2014-10-20 19:00:00", $eventCore->getStartDateTime()->formatDateTime());
+
+        $eventDetail = $event->getEventDetail();
+        $this->assertEquals("17662", $eventDetail->getSourceEventId());
+        $this->assertEquals("概要1", $eventDetail->getEventDescription());
+        $this->assertEquals("キャッチコピー1", $eventDetail->getCatchCopy());
+        $this->assertEquals("10000", $eventDetail->getOwnerId());
+
+        $eventGeolocation = $event->getEventGeolocation();
+        $this->assertEquals("東京都千代田区丸の内3丁目5番1号", $eventGeolocation->getAddress());
+        $this->assertEquals("東京国際フォーラム　ホールB7", $eventGeolocation->getPlace());
+        $this->assertEquals("35.6769467", $eventGeolocation->getLatitude());
+        $this->assertEquals("139.7635034", $eventGeolocation->getLongitude());
+
+        $eventCapacity = $event->getEventCapacity();
+        $this->assertEquals("30", $eventCapacity->getCapacityLimit());
+        $this->assertEquals("5", $eventCapacity->getAccepted());
+        $this->assertEquals("0", $eventCapacity->getWaiting());
     }
 
 }
