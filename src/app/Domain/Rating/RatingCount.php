82fd7781e3324755b7d8722b215d5dab30f82962
tmknom <tmk.nom@gmail.com>
2015-05-09 02:22:13 +0900

    RatingCountを移動

---
+++ src/app/Domain/Rating/RatingCount.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace App\Domain\Rating;
+
+interface RatingCount
+{
+
+}
--- src/app/Domain/Rating/RatingCount/HatenaBookmarkCount.php
+++ src/app/Domain/Rating/RatingCount/HatenaBookmarkCount.php
@@ -2,6 +2,7 @@
 
 namespace App\Domain\Rating\RatingCount;
 
+use App\Domain\Rating\RatingCount;
 use App\Library\Domain\ValueObject;
 
 class HatenaBookmarkCount implements RatingCount
