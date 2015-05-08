88bb8503a2cc996b2c0a722eb0b5cfd9d6e4fff7
tmknom <tmk.nom@gmail.com>
2015-05-08 17:09:52 +0900

    SocialCrawlerCommandTest作成

---
+++ src/tests/Console/Commands/SocialCrawler/SocialCrawlerCommandTest.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Tests\Console\Commands\SocialCrawler;
+
+use App\Application\EventUrlListReference\EventUrlListReferenceApplication;
+use App\Application\SocialCrawler\TwitterCrawlerApplication;
+use App\Console\Commands\SocialCrawler\SocialCrawlerCommand;
+use App\Library\Http\JsonHttpClient;
+use Tests\Base\TestCase;
+use Tests\Fixture\Seeder\EventRatingSeeder;
+use Tests\Fixture\Seeder\EventSeeder;
+use Tests\Infrastructure\SocialCrawler\Stub\TwitterJsonHttpClient;
+
+class SocialCrawlerCommandTest extends TestCase
+{
+
+    /** @var SocialCrawlerCommand */
+    private $sut;
+
+    /** @before */
+    public function setUp()
+    {
+        parent::setUp();
+
+        $this->app->bind(JsonHttpClient::class, TwitterJsonHttpClient::class);
+        $eventUrlListReferenceApplication = $this->app->make(EventUrlListReferenceApplication::class);
+        $crawlerApplication = $this->app->make(TwitterCrawlerApplication::class);
+        $this->sut = new SocialCrawlerCommand($eventUrlListReferenceApplication, $crawlerApplication);
+    }
+
+    /** @test */
+    public function fire_正常系()
+    {
+        // 事前準備：DBに初期データをセット
+        $this->seed(EventSeeder::class);
+        $this->seed(EventRatingSeeder::class);
+
+        // 事前準備：標準出力をバッファリング
+        ob_start();
+
+        // 実行
+        $this->sut->fire();
+
+        // 確認
+        $this->assertEquals('crawler:social', $this->sut->getName());
+        // 確認＆後始末：バッファリングした標準出力を取得して、バッファリングは終了
+        $actual = ob_get_clean();
+        $this->assertEquals('success', $actual);
+    }
+
+}
