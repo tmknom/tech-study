04216008c2946d342050eae2816701b6fc6ef166
tmknom <tmk.nom@gmail.com>
2015-05-09 18:49:31 +0900

    CrawlerCommandの出力形式変更

--- src/app/Console/Commands/EventCrawler/CrawlerCommand.php
+++ src/app/Console/Commands/EventCrawler/CrawlerCommand.php
@@ -38,7 +38,12 @@ trait CrawlerCommand
         $startTime = microtime(true);
         $eventList = $crawlerApplication->crawl();
         $executeTime = number_format(microtime(true) - $startTime, 3);
-        echo json_encode(['event_count' => $eventList->count(), 'execute_time' => $executeTime]) . PHP_EOL;
+        $commandName = str_replace('CrawlerCommand', '', array_slice(explode('\\', get_called_class()), -1)[0]);
+        echo json_encode([
+            'type' => $commandName,
+            'event_count' => $eventList->count(),
+            'execute_time' => $executeTime,
+        ]) . PHP_EOL;
     }
 
 }
