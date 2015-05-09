b81ccd9cccbe2173963563ebf751a165e4298697
tmknom <tmk.nom@gmail.com>
2015-05-09 09:33:25 +0900

    FacebookCrawlerApplicationリファクタ

--- src/app/Application/SocialCrawler/FacebookCrawlerApplication.php
+++ src/app/Application/SocialCrawler/FacebookCrawlerApplication.php
@@ -2,45 +2,23 @@
 
 namespace App\Application\SocialCrawler;
 
-use App\Domain\Event\Core\EventUrl;
-use App\Domain\Event\EventRepository;
-use App\Domain\Rating\RatingCount\FacebookCount;
+use App\Domain\Rating\Repository\FacebookRatingRepository;
 use App\Domain\SocialCrawler\FacebookCountCrawler;
-use DB;
 
 class FacebookCrawlerApplication
 {
 
-    /**  @var FacebookCountCrawler */
-    private $ratingCountCrawler;
-
-    /**  @var EventRepository */
-    private $eventRepository;
-
-    /**  @var FacebookCount */
-    private $updatedCount;
-
-    /** コンストラクタ */
-    public function __construct(FacebookCountCrawler $ratingCountCrawler, EventRepository $eventRepository)
-    {
-        $this->ratingCountCrawler = $ratingCountCrawler;
-        $this->eventRepository = $eventRepository;
-        $this->updatedCount = null;
-    }
+    use TransactionSocialCrawlerApplication;
 
     /**
-     * @param EventUrl $eventUrl
-     * @return FacebookCount
+     * コンストラクタ
+     *
+     * @param FacebookCountCrawler $socialCrawler
+     * @param FacebookRatingRepository $ratingRepository
      */
-    public function crawl(EventUrl $eventUrl)
+    public function __construct(FacebookCountCrawler $socialCrawler, FacebookRatingRepository $ratingRepository)
     {
-        DB::transaction(function () use ($eventUrl) {
-            // ソーシャルカウント取得
-            $this->updatedCount = $this->ratingCountCrawler->crawl($eventUrl);
-            // DB更新
-            $this->eventRepository->saveFacebookCount($eventUrl, $this->updatedCount);
-        });
-        return $this->updatedCount;
+        $this->construct($socialCrawler, $ratingRepository);
     }
 
 }
