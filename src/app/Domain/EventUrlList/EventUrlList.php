21d82e3bc19e243c22412fc35c68dab1e57fb7c4
tmknom <tmk.nom@gmail.com>
2015-05-10 14:09:14 +0900

    DomainCollectionリファクタ

--- src/app/Domain/EventUrlList/EventUrlList.php
+++ src/app/Domain/EventUrlList/EventUrlList.php
@@ -3,14 +3,10 @@
 namespace App\Domain\EventUrlList;
 
 use App\Library\Domain\DomainCollection;
-use Illuminate\Support\Collection;
 
 class EventUrlList
 {
 
     use DomainCollection;
-
-    /** @var Collection */
-    private $value;
 
 }
